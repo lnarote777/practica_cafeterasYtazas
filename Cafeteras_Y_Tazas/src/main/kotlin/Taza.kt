64f345ed4ba20235e71ec036a0d6dadf57c15518
lnarote777 <nolara@iesrafaelalberti.es>
2024-01-23 19:14:20 +0100

    cafetera y tazas

---
+++ Cafeteras_Y_Tazas/src/main/kotlin/Taza.kt
@@ -0,0 +1,25 @@
+class Taza(private var color: Color = Color.Blanco, private var capacidad: Int = 50, private var cantidad: Int = 0){
+
+    fun getColor(): Color {
+        return this.color
+    }
+    fun setColor(color: Color){
+        this.color = color
+    }
+    init {
+        if (this.capacidad < this.cantidad){
+            this.cantidad = this.capacidad
+        }
+    }
+    fun llenar(){
+        this.cantidad = this.capacidad
+    }
+//    fun llenar(Int){
+//
+//    }
+
+    override fun toString(): String {
+        return "Taza(color = ${this.color}, capacidad = ${this.capacidad}c.c., cantidad = ${this.cantidad} )"
+    }
+
+}
---
+++ Cafeteras_Y_Tazas/src/main/kotlin/cafetera.kt
@@ -0,0 +1,29 @@
+class Cafetera(var ubicacion: String){
+
+    var capacidad: Int = 1000 //Capacidad máxima de la cafetera
+    var cantidad : Int = 0 //Cantidad actual en la cafetera
+
+    constructor(ubicacion: String, capacidad: Int): this(ubicacion){
+        this.cantidad = capacidad
+    }
+    constructor(ubicacion: String, capacidad: Int, cantidad: Int ) : this(ubicacion){
+        this.capacidad = capacidad
+        if(cantidad > capacidad ){
+            this.cantidad = this.capacidad
+        }else{
+            this.cantidad = cantidad
+        }
+    }
+
+    fun llenar(){
+        this.cantidad = this.capacidad
+    }
+    fun servirTaza(){
+
+    }
+
+    override fun toString(): String {
+        return "Cafetera(ubicación = ${this.ubicacion}, capacidad = ${this.capacidad}c.c., cantidad = ${this.cantidad} )"
+    }
+
+}
